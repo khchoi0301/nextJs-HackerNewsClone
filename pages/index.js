dd1517faf1d3d56e01d4a72d51a49c62e6ff925e
khchoi0301 <khchoi0301@gmail.com>
2020-03-03 02:52:33 +0900

    Handling Errors with built-in Error Page and Try/Catch

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch'
+import Error from 'next/error'
 
 class Index extends Component {
+    static async getInitialProps() {
+        let stories = [];
+        try {
+            const response = await fetch("https://node-hnapi.herokuapp.com/news?page=1");
+            stories = await response.json()
+        } catch (error) {
+            console.log(error)
+        }
+        return { stories }
+    }
+
     render() {
-        return (
-            <div>
+        const { stories } = this.props
+
+        if (stories.length === 0) {
+            return <Error statusCode={503} />
+        }
+
+        return <div>
+            <h1>
                 hacker next
+                </h1>
+            <div>
+                {stories.map(story => (<h2>{story.title}</h2>))}
             </div>
-        );
+        </div>;
     }
 }
 
